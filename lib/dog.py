fa95ed056ee9f10244c375231309eaa83b20fd14
LeightonSkervin <lskervin23@gmail.com>
2024-03-13 15:09:32 -0400

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,12 @@
 
 class Dog:
     # Class body goes here
-
+    def bark(self):
+        print("Woof!")
     #Instance method definition
+    def sit(self):
+        print("The dog is sitting.")
     pass
+
+fido = Dog()
+fido.sit()
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,13 @@
 
 class Person:
     # Class body goes here
+    def __init__(self):
+        pass
 
     #Instance method definition
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     pass
